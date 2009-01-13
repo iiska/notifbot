a9216ec668fa84ed4835922b5488b4a25c243e4c
Juhamatti Niemelä <iiska@iki.fi>
2009-01-13 20:00:48 +0200

    Configuration for SimpleRss.

--- plugins/simple_rss.rb
+++ plugins/simple_rss.rb
@@ -3,22 +3,23 @@ require 'open-uri'
 require 'rss'
 
 class SimpleRss < Plugin
-  def initialize(bot)
+  def initialize(bot, config)
     @bot = bot;
 
-    @url = "http://www.raippa.fi/RecentChanges?action=rss_rc&unique=1"
+    @threads = []
 
     # Main thread of the rss reader, which polls given url
-    @thread = Thread.new(@bot, @url) do |bot, url|
+    config['urls'].each{|u|
+      @threads.push Thread.new(@bot, u) do |bot, url|
 
-      open(url) {|f|
-        feed = RSS::Parser.parse(f.read, false)
-        feed.items.each{|item|
-          puts "#{feed.title}: #{item.title}", item.link
-          #bot.plugin_post item.title
+        open(url) {|f|
+          feed = RSS::Parser.parse(f.read, false)
+          feed.items.each{|item|
+            puts "#{feed.title}: #{item.title}", item.link
+            #bot.plugin_post item.title
+          }
         }
-      }
-      sleep(60)
-    end
+        sleep(60)
+      end
   end
 end
