b346cfae788df149eac8bafc0aacfca328ec64f0
Juhamatti Niemelä <iiska@iki.fi>
2009-01-13 19:52:08 +0200

    Use open-uri.

--- plugins/simple_rss.rb
+++ plugins/simple_rss.rb
@@ -1,26 +1,24 @@
 
-require 'net/http'
-require 'net/https'
+require 'open-uri'
 require 'rss'
 
 class SimpleRss < Plugin
   def initialize(bot)
     @bot = bot;
 
-    @url = URI.parse("http://www.raippa.fi/RecentChanges?action=rss_rc&unique=1&ddiffs=1")
-    @http = Net::HTTP.new(@url.host, @url.port)
-    # TODO: Check if https, and then use SSL
-    # @http.use_ssl = true
-    # @http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    @url = "http://www.raippa.fi/RecentChanges?action=rss_rc&unique=1"
 
     # Main thread of the rss reader, which polls given url
-    @thread = Thread.new(@bot, @http, @url) do |bot, http, url|
-      req = Net::HTTP::Get.new(url.path)
-      feed = RSS::Parser.parse(http.request(req).body, false)
+    @thread = Thread.new(@bot, @url) do |bot, url|
 
-      feed.items.each{|item|
-        bot.plugin_post item.title
+      open(url) {|f|
+        feed = RSS::Parser.parse(f.read, false)
+        feed.items.each{|item|
+          puts "#{feed.title}: #{item.title}", item.link
+          #bot.plugin_post item.title
+        }
       }
+      sleep(60)
     end
   end
 end
