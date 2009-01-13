63bd289cce935dabf15cd06b21d831ec52d8020f
Juhamatti Niemelä <iiska@iki.fi>
2009-01-13 18:57:39 +0200

    Initial commit.

---
+++ notif_bot.rb
@@ -0,0 +1,45 @@
+#! /usr/bin/env ruby
+
+require 'rubygems'
+require 'net/irc' # net-irc gem
+require 'yaml'
+
+require 'plugin'
+
+# Parse a few random irc log files from the directories given
+# as command line parameters.
+class NotifBot < Net::IRC::Client
+  def initialize(config_file)
+    @config = YAML::load(File.open(config_file))
+
+    super(@config['server'],@config['port'], {
+            :nick => @config['nick'],
+            :user => @config['nick'],
+            :real => @config['realname']
+          })
+  end
+
+  def on_message(m)
+    super
+    # This may be network dependant but at least in IRCnet End of MOTD
+    # is command 376.
+    #if (/End of MOTD/.match(m) and (@joined_channels == []))
+    if (m.command == '376') and (@joined_channels == [])
+      post JOIN, @config['channel']
+      @joined_channels << @config['channel']
+    else
+    end
+  end
+end
+
+config_file = ['./config.yml',
+               './notifbotrc',
+               '~/.notifbotrc'].select{|f|
+  f && File.file?(f)
+}.first
+
+if config_file
+  NotifBot.new(config_file).start
+else
+  puts "No config file found."
+end
